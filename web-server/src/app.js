4cf2186a1ee08ecb1a38e4551bc55e1e8e80b140
Adil Ausagit <6kdoter@gmail.com>
2020-08-04 22:25:51 +0600

    basic routes and templates added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,59 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('asd')
-})
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render("index", {
+        title: 'Weather App',
+        name: 'dim'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render("about", {
+        title: 'yolo',
+        name: 'vitalya'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render("help", {
+        title: 'helppp',
+        name: 'lyk'
+    })
 })
 
 app.get('/weather', (req, res) => {
     res.send('Weather page')
 })
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'asd',
+        name: 'asd',
+        errorMessage: 'Help page not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'asd',
+        name: 'asd',
+        errorMessage: 'Page not found'
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('Server is up')
